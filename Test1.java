3ae28e4a1239ba8d374be1f8b41fd641b0f131c8
javaexpresz <javaexpresz@gmail.com>
2022-11-04 18:19:25 +0530

    new file added

---
+++ Test1.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Test1 {
+public static void main(String[] args) {
+	Set set=new HashSet();
+	set.add("deepak");
+	set.add("ravi");
+	set.add("neha");
+	set.add("avi");
+	set.add("amit");
+	System.out.println(set);
+}
+}
